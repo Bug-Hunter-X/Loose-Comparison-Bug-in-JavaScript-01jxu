a5629e841cc67c2df5300693fb4a6b0bec86de60
BugHunter-X <askzenai@gmail.com>
2025-01-03 00:44:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,4 @@
+function foo(a,b){    if (a == 1 && b == 2) {        return true;    } else {        return false;    }}
+
+console.log(foo(1,2)); // true
+console.log(foo(1,"2")); //false, expected true because of loose comparison
